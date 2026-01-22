2d1d31212248a8cd2bc989c5feb43a831bdd8f73
Gustavo Gomes <devgustavogomes31@gmail.com>
2026-01-22 16:16:46 -0300

    feat: refactor guard and decorator owner to checking not only id params of resources

--- src/shared/decorators/owner.decorator.ts
+++ src/shared/decorators/owner.decorator.ts
@@ -1,5 +1,5 @@
 import { SetMetadata } from '@nestjs/common';
 
 export const OWNER_SERVICE_KEY = 'ownerService';
-export const OwnerService = (service: object) =>
-  SetMetadata(OWNER_SERVICE_KEY, service);
+export const OwnerService = (service: object, paramsKey?: string) =>
+  SetMetadata(OWNER_SERVICE_KEY, { service, paramsKey });
